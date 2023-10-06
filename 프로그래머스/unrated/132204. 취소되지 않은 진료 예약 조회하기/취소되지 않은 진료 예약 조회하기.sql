78ccc3b4b7d502037d401808ce46b9c26fe01c4f
yangsae912 <104888233+yangsae912@users.noreply.github.com>
2023-10-06 11:31:31 +0900

    [unrated] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,22 +1,17 @@
 -- 코드를 입력하세요
--- select a.apnt_no, p.pt_name, p.pt_no, a.mcdp_cd, d.dr_name, a.apnt_ymd
--- from patient p 
--- inner join appointment a 
--- on p.pt_no = a.pt_no
--- inner join doctor d 
--- on a.mcdp_cd = d.mcdp_cd
--- where to_char(a.apnt_ymd, 'YYYY-MM-DD') = '2022-04-13'
--- and a.apnt_cncl_yn = 'N'
--- and d.mcdp_cd ='CS'
--- order by a.apnt_ymd asc
-
-
-
-SELECT A.APNT_NO, P.PT_NAME, A.PT_NO, D.MCDP_CD, D.DR_NAME, A.APNT_YMD
-FROM APPOINTMENT A
-INNER JOIN PATIENT P ON A.PT_NO = P.PT_NO 
-INNER JOIN DOCTOR  D ON A.MDDR_ID = D.DR_ID
-WHERE to_char(A.APNT_YMD,'YYYY-MM-DD') = '2022-04-13' 
-AND A.APNT_CNCL_YN = 'N'
-AND A.MCDP_CD = 'CS'
-ORDER BY APNT_YMD ASC
+select a.apnt_no, p.pt_name, a.pt_no, d.mcdp_cd, d.dr_name, a.apnt_ymd
+from appointment a 
+inner join patient p on a.pt_no = p.pt_no 
+inner join doctor d  on a.mddr_id = d.dr_id
+where to_char(a.apnt_ymd, 'YYYY-MM-DD') = '2022-04-13'
+and a.apnt_cncl_yn = 'N'
+and a.mcdp_cd ='CS'
+order by a.apnt_ymd asc
+-- SELECT A.APNT_NO, P.PT_NAME, A.PT_NO, D.MCDP_CD, D.DR_NAME, A.APNT_YMD
+-- FROM APPOINTMENT A
+-- INNER JOIN PATIENT P ON A.PT_NO = P.PT_NO 
+-- INNER JOIN DOCTOR  D ON A.MDDR_ID = D.DR_ID
+-- WHERE to_char(A.APNT_YMD,'YYYY-MM-DD') = '2022-04-13' 
+-- AND A.APNT_CNCL_YN = 'N'
+-- AND A.MCDP_CD = 'CS'
+-- ORDER BY APNT_YMD ASC
