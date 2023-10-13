2a31b9be7d09fc9a604c8fc30539c7ec79973c89
yangsae912 <104888233+yangsae912@users.noreply.github.com>
2023-10-13 13:17:29 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,4 +1,4 @@
 def solution(numbers):
     answer = [i for i in range(10)]
-    difference = list(filter(lambda x: x not in numbers, answer))
-    return sum(diff for diff in difference)
+    #answer 리스트의 원소 중, numbers 리스트에 없는 원소 x 에 대해 filter()하고 이를 list에 넣는다 
+    return sum(filter(lambda x: x not in numbers, answer))
