da49e09b57d3f4f0b898b897cf691b4e2ead6746
yangsae912 <104888233+yangsae912@users.noreply.github.com>
2023-10-29 20:16:59 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,10 +1,20 @@
+# def solution(a,b):
+#     day = ['FRI','SAT','SUN','MON','TUE','WED','THU']
+#     month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+#     mon_day = []
+#     for index, mon in enumerate(month, start = 1 ):
+#         mon_day.append((index, mon))
+#     result = [ (day) for mon, day in mon_day[:a-1]] 
+    
+#     return day[(sum(result) + b) % 7 - 1]
+
 def solution(a,b):
+    
     day = ['FRI','SAT','SUN','MON','TUE','WED','THU']
     month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-    mon_day = []
-    for index, mon in enumerate(month, start = 1 ):
-        mon_day.append((index, mon))
-    result = [ (day) for mon, day in mon_day[:a-1]] 
     
-    return day[(sum(result) + b) % 7 - 1]
+    if a > 1:
+        return day[(sum(month[:a - 1]) + b) % len(day) - 1]
+    else:
+        return day[ b % len(day) - 1]
