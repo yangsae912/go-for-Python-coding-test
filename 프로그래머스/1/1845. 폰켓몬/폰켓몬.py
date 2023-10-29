e876248ef5fc30ff6eab7d2d4003356bc0eecd28
yangsae912 <104888233+yangsae912@users.noreply.github.com>
2023-10-29 20:44:02 +0900

    [level 1] Title: 폰켓몬, Time: 0.40 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,5 +1,12 @@
-def solution(nums):
-    max_len =  len(set(nums))
-    div = len(nums) // 2
+# def solution(nums):
+#     max_len =  len(set(nums))
+#     div = len(nums) // 2
     
-    return min(max_len, div)
+#     return min(max_len, div)
+
+
+def solution(nums):
+    if len(list(set(nums))) < len(nums) //2:
+        return len(list(set(nums)))
+    else:
+        return len(nums) // 2 
