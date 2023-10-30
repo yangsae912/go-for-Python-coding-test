5fa42b0299469cd31c1d5e4b4f6fa6d02e765172
yangsae912 <104888233+yangsae912@users.noreply.github.com>
2023-10-30 09:59:14 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,37 +1,52 @@
-def is_prime(num):
-    if num <= 1:
-        return False
-    elif num <= 3:
-        return True
-    elif num % 3 == 0 or num % 2 == 0:
-        return False
+# def is_prime(num):
+#     if num <= 1:
+#         return False
+#     elif num <= 3:
+#         return True
+#     elif num % 3 == 0 or num % 2 == 0:
+#         return False
 
-    i = 5
-    while i * i <= num:
-        if num % i == 0 or num % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+#     i = 5
+#     while i * i <= num:
+#         if num % i == 0 or num % (i + 2) == 0:
+#             return False
+#         i += 6
+#     return True
 
-def generate_primes(limit):
-    primes = []
+# def generate_primes(limit):
+#     primes = []
     
-    for num in range(1, limit+1):
-        if is_prime(num):
-            primes.append(num)
-    return primes
+#     for num in range(1, limit+1):
+#         if is_prime(num):
+#             primes.append(num)
+#     return primes
 
 
+# def solution(nums):
+    
+#     from itertools import combinations
+
+    
+#     primes = generate_primes(2997)
+#     cnt = 0
+#     for num in list(combinations(nums,3)):
+#         if sum(num) in primes:
+#             cnt +=1
+#     return cnt
+    
 def solution(nums):
-    
     from itertools import combinations
-
-    
-    primes = generate_primes(2997)
     cnt = 0
-    for num in list(combinations(nums,3)):
-        if sum(num) in primes:
-            cnt +=1
-    return cnt
-    
-    
+    for i in combinations(nums,3):
+        if is_div(sum(i)):
+            cnt += 1
+    return cnt 
+
+def is_div(n):
+    result = 0
+    for number in range(1, n+1):
+        if n % number == 0: result += 1
+    if result >= 3:
+        return False
+    else:
+        return True    
