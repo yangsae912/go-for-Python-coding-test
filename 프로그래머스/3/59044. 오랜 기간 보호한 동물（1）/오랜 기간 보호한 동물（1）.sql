39222c1fa74b01fc806348f5acbf7d93c8d333b6
양세연 <104888233+yangsae912@users.noreply.github.com>
2024-01-24 18:06:27 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,9 +1,9 @@
--- 코드를 입력하세요
-select ins.name, ins.datetime
-from animal_ins ins
-left outer join animal_outs outs
-on ins.animal_id = outs.animal_id
-where outs.name is null
-and ins.name is not null
-order by 2
-fetch first 3 rows only
+-- 2023.1.24
+
+SELECT I.NAME, I.DATETIME
+FROM ANIMAL_INS I
+LEFT JOIN ANIMAL_OUTS O ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE O.ANIMAL_ID IS NULL 
+ORDER BY 2 ASC 
+FETCH FIRST 3 ROWS ONLY;
+
