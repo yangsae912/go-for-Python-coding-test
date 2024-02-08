497544a1ce297828149ffc001eaf0aa16713ef2e
양세연 <104888233+yangsae912@users.noreply.github.com>
2024-02-08 17:42:58 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,17 +1,11 @@
--- 코드를 입력하세요
--- SELECT '/home/grep/src/'||f.board_id|| '/'|| file_id || file_ext AS FILE_PATH
--- from used_goods_board b
--- inner join used_goods_file f 
--- on b.board_id = f.board_id
--- order by views desc , file_id
--- fetch first 1 rows only
--- -- order by file_id desc
-
-
-select '/home/grep/src/'||board_id|| '/'|| file_id ||file_name || file_ext  AS FILE_PATH
-from used_goods_file
-where board_id = (select board_id
-from USED_GOODS_BOARD
-order by views desc
-fetch first 1 rows only)
+-- -- 코드를 입력하세요
+select '/home/grep/src/' || a.board_id ||'/'|| file_id || file_name || file_ext as file_path
+from USED_GOODS_FILE a 
+left join USED_GOODS_BOARD b on a.board_id = b.board_id
+where views =  
+    (
+        select max(VIEWS)
+        from USED_GOODS_BOARD
+        
+    )
 order by file_id desc
